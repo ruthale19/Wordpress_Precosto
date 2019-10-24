a8460b7bb03eb7f147f91dc00c2428cfb43929a2
Ruth <ruthale@gmail.com>
2019-10-23 21:02:51 -0300

    fourteenth commit Wordpress project single-posttype

--- themes/mi_tema/single-project.php
+++ themes/mi_tema/single-project.php
@@ -14,33 +14,33 @@
 			<a href="<?php echo home_url(); ?>" class="btn btn-color btn-letter btn-sm my-5 mr-2">Volver a <i class="fas fa-home"></i></a>
 			<a href="proyectos" class="btn btn-color btn-letter btn-sm my-5">Volver a proyectos <i class="far fa-folder-open"></i></a>
 
-				<h3 class="text-center my-5 font-weight-bold"> <?php the_field( 'obra' ); ?> </h3>
+				<h3 class="text-center my-5 font-weight-bold single-project__text1"> <?php the_field( 'obra' ); ?> </h3>
 
 				<div class="text-center my-5">
 					<?php $image1 = get_field( 'image1' ); ?>
 					<?php if ( $image1 ) { ?>
-						<img src="<?php echo $image1['url']; ?>" alt="<?php echo $image1['alt']; ?>" style="width: 1000px; height: 400px;"/>
+						<img src="<?php echo $image1['url']; ?>" alt="<?php echo $image1['alt']; ?>" style="width: 600px; height: 400px;"/>
 					<?php } ?>
 				</div>
 
 				<div class="text-center my-5">
 					<?php $image2 = get_field( 'image2' ); ?>
 					<?php if ( $image2 ) { ?>
-						<img src="<?php echo $image2['url']; ?>" alt="<?php echo $image2['alt']; ?>" style="width: 1000px; height: 400px;"/>
+						<img src="<?php echo $image2['url']; ?>" alt="<?php echo $image2['alt']; ?>" style="width: 600px; height: 400px;"/>
 					<?php } ?>
 				</div>
 
 				<div class="text-center my-5">
 					<?php $image3 = get_field( 'image3' ); ?>
 					<?php if ( $image3 ) { ?>
-						<img src="<?php echo $image3['url']; ?>" alt="<?php echo $image3['alt']; ?>" style="width: 1000px; height: 400px;"/>
+						<img src="<?php echo $image3['url']; ?>" alt="<?php echo $image3['alt']; ?>" style="width: 600px; height: 400px;"/>
 					<?php } ?>
 				</div>
 
 				<div class="text-center my-5">
 					<?php $image4 = get_field( 'image4' ); ?>
 					<?php if ( $image4 ) { ?>
-						<img src="<?php echo $image4['url']; ?>" alt="<?php echo $image['alt']; ?>" style="width: 1000px; height: 400px;"/>
+						<img src="<?php echo $image4['url']; ?>" alt="<?php echo $image['alt']; ?>" style="width: 600px; height: 400px;"/>
 					<?php } ?>
 				</div>
 
